c5ab273b108a3aefba58b2d7033eef1582909d58
Hugo de Grouchy <hugodegrouchy@gmail.com>
2022-10-12 09:08:02 +0100

    feat: add loader and good redirections

--- front_end_flutter/lib/Components/button.dart
+++ front_end_flutter/lib/Components/button.dart
@@ -7,11 +7,13 @@ class Button extends StatefulWidget {
     required this.text,
     required this.onPress,
     this.secondary,
+    this.isLoading,
   });
 
   final String text;
   final void Function()? onPress;
   final bool? secondary;
+  final bool? isLoading;
 
   @override
   ButtonState createState() => ButtonState();
@@ -34,21 +36,29 @@ class ButtonState extends State<Button> {
         height: 54,
         decoration: BoxDecoration(
           borderRadius: const BorderRadius.all(Radius.circular(16)),
-          color: widget.secondary != null ? white : grisfoncer,
+          color: widget.secondary == true ? white : grisfoncer,
           boxShadow: const <BoxShadow>[
             BoxShadow(blurRadius: 16, color: grisclairetextfield)
           ],
         ),
         child: TextButton(
           onPressed: _onButtonPress,
-          child: Text(
-            widget.text,
-            style: TextStyle(
-              color: widget.secondary != null ? grisfoncer : white,
-              fontFamily: "bold",
-              fontSize: 16,
-            ),
-          ),
+          child: widget.isLoading == true
+              ? SizedBox(
+                  width: 20,
+                  height: 20,
+                  child: CircularProgressIndicator(
+                    color: widget.secondary == true ? grisfoncer : white,
+                  ),
+                )
+              : Text(
+                  widget.text,
+                  style: TextStyle(
+                    color: widget.secondary != null ? grisfoncer : white,
+                    fontFamily: "bold",
+                    fontSize: 16,
+                  ),
+                ),
         ),
       ),
     );
