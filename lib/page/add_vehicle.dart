16de4f30bdd0f09f3ec0c2583538492ec8b4eed3
Lucile <lucile.remigy@epitech.eu>
2022-11-12 10:59:07 +0100

    fix: name app and calendar in vehicule add

--- lib/page/add_vehicle.dart
+++ lib/page/add_vehicle.dart
@@ -596,14 +596,12 @@ class _AddVehicule extends State<AddVehicule> {
                                         onTap: () async {
                                           final DateTime? pickedDate =
                                               await showDatePicker(
-                                            initialEntryMode:
-                                                DatePickerEntryMode.input,
                                             context: context,
                                             initialDate: DateTime.now(),
                                             firstDate: DateTime(
-                                              2000,
+                                              1900,
                                             ),
-                                            lastDate: DateTime(2101),
+                                            lastDate: DateTime(2100),
                                           );
 
                                           if (pickedDate != null) {
@@ -674,9 +672,9 @@ class _AddVehicule extends State<AddVehicule> {
                                         context: context,
                                         initialDate: DateTime.now(),
                                         firstDate: DateTime(
-                                          2000,
+                                          1900,
                                         ),
-                                        lastDate: DateTime(2101),
+                                        lastDate: DateTime(2100),
                                       );
 
                                       if (pickedDate != null) {
